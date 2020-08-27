157e732a3eac975ea08ef3518a4faf48d39dca82
dianest <nddgdd@gmail.com>
2020-08-27 12:39:58 +0100

    Change variable for output

--- src/com/netology/hw/Main.java
+++ src/com/netology/hw/Main.java
@@ -4,12 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         int currentBalance = 100;
-        int transferMoney = 1001;
+        int transferMoney = 15001;
         int bonusMoney = 0;
         if (transferMoney > 1000) {
             bonusMoney = transferMoney / 100;
         }
-        int finalBalance = currentBalance + transferMoney + bonusMoney;
-        System.out.println(finalBalance);
+        System.out.println(bonusMoney);
     }
 }
